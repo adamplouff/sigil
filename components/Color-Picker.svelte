220fceb4e2fe7648af4b38b30fc8be6ca79880ce
Adam Plouff <adamplouff@gmail.com>
2023-02-03 11:32:04 -0500

    Create a picker comp if comp isn't open

--- components/Color-Picker.svelte
+++ components/Color-Picker.svelte
@@ -1,88 +1,138 @@
 <script>
     import { createEventDispatcher } from 'svelte'
     import { fade } from 'svelte/transition';
-
-    import ColorPicker from 'svelte-awesome-color-picker';
-	let rgb; // or hsv or hex
+    import {
+        evalES,
+    } from "../lib/utils";
 
     const dispatch = createEventDispatcher();
 
     export let color = ''
     export let label = ''
-    // export let tooltip = ''
+    // export let js = false
 
-    $: isOpen = false
+    const sanitizeHex = (hexColor) => {
+        const hexString = hexColor.replace('#', '')
+        if (hexString.length < 6) {     // catch 3 digit hex 
+            return `#${hexString[0]}${hexString[0]}${hexString[1]}${hexString[1]}${hexString[2]}${hexString[2]}`
+        } else {
+            return `#${hexString}`
+        }
+    }
+    let _color = sanitizeHex(color) 
+    console.log('_color', _color);
+    
+    const hexToArray = (hexString) => {
+        const hexColor = hexString.replace('#', '');
+        const r = parseInt(hexColor.slice(0, 2), 16) / 255 || 0,
+        g = parseInt(hexColor.slice(2, 4), 16) / 255 || 0,
+        b = parseInt(hexColor.slice(4, 6), 16) / 255 || 0;
+        return [r, g, b, 1];
+    }
+    function arrayToHex(colorArray) {
+        const red = parseInt(colorArray[0] * 255)
+        const green = parseInt(colorArray[1] * 255)
+        const blue = parseInt(colorArray[2] * 255)
 
-    import { createPopperActions } from 'svelte-popperjs';
-    const [popperRef, popperContent] = createPopperActions({
-        placement: 'bottom',
-        strategy: 'fixed',
-        applyStyles: true,
-    });
-    const extraOpts = {
-        modifiers: [
-            { name: 
-                'offset', 
-                options: { 
-                    offset: [0, 4] ,
-                } 
-            }
-        ],
+        return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`;
+    };
+    const parseColorArray = (colorString) => {
+        if (colorString) {
+            const colorArray = JSON.parse(`[${colorString}]`)
+            console.log('colorArray', colorArray);
+            const colorUpdate = arrayToHex(colorArray)
+            console.log('colorUpdate', colorUpdate);
+            _color = colorUpdate
+            dispatch('update', {state: arrayToHex(colorArray)})
+        }
     }
 
-    const closeModal = () => {
-        // dispatch('close')
-        isOpen = false
+    const colorScript = () => {
+
+        return `var thisComp = app.project.activeItem
+var color = [${ hexToArray(color) }];
+if (!thisComp) {
+    var pickerComp = app.project.items.addComp('Picker', 100, 100, 1, 5, 24);
+    pickerComp.openInViewer();
+    thisComp = pickerComp
+    var retColor = pickColor(pickerComp, color)
+    pickerComp.remove()
+    retColor
+} else {
+    var retColor = pickColor(thisComp, color)
+    retColor
+}
+
+function pickColor(comp, color) {
+    var pickerLayer = comp.layers.addShape();
+    pickerLayer.name = 'Picker';
+    var fill = pickerLayer('ADBE Root Vectors Group').addProperty('ADBE Vector Graphic - Fill');
+    var fillColorProp = fill('ADBE Vector Fill Color');
+    fillColorProp.setValue(color);
+    fillColorProp.selected = true;
+    app.executeCommand(2240);
+    
+    pickedColor = fillColorProp.value;
+    pickerLayer.remove()
+    return pickedColor
+}`
+        // return `
+        // var retColor = [1,1,1,1];
+        // try {
+        // var thisComp = app.project.activeItem
+        // if (!thisComp) {
+        //     var thisComp = app.project.items.addComp('Picker', 100, 100, 1, 5, 24);
+        //     pickerComp.openInViewer()
+        //     pickColor(pickerComp, [${ hexToArray(_color) }]
+        // }
+        // // var pickerComp = app.project.activeItem
+        // if (thisComp) {
+        //     //if (!thisComp || !(thisComp instanceof CompItem)) { return false }
+        //     // app.beginUndoGroup('Color picker');
+            
+        //     var pickerLayer = thisComp.layers.addShape();
+        //     pickerLayer.name = 'Picker';
+        //     var fill = pickerLayer('ADBE Root Vectors Group').addProperty('ADBE Vector Graphic - Fill');
+        //     var fillColorProp = fill('ADBE Vector Fill Color');
+        //     // fillColorProp.setValue([1,0,1,1]);
+        //     fillColorProp.setValue([${ hexToArray(_color) }]);
+        //     fillColorProp.selected = true;
+        //     app.executeCommand(2240);
+        //     retColor = fillColorProp.value;
+        //     // app.endUndoGroup();
+        //     // if (pickerLayer) {
+        //     //     pickerLayer.remove();
+        //     // }
+        //     // app.executeCommand(2371);
+        //     // thisComp.remove()
+        //     retColor
+        // } else {
+        //     null;
+        // }
+
+        // } catch (e) { alert(e.toString() + "Error on line: " + e.line.toString());}
+        
+        // function pickColor(comp, starringColor) {
+            
+        // }
+        // `
     }
 </script>
 
-{#if open}
-<div class="scrim" on:click={ closeModal } />
-{/if}
-
-{ isOpen }
-
-<div on:click={ () => isOpen = true }>
-    <ColorPicker 
-        bind:rgb 
-        label={ label } 
-        hex={ color }
-        isAlpha={ false } 
-        canChangeMode={ false } 
-    />
-</div>
-
-<!-- <div class="container" 
-    on:mouseenter={ () => hover = true }
-    on:mouseleave={ () => hover = false }
-    on:click={ () => dispatch('click') }
->
+<div class="picker-wrapper" on:click={() => evalES(colorScript(), true).then(data => parseColorArray(data))}>
     <div class="swatch">
         <div class="swatch-color" 
-        style="background: {color}"/>
+        style="background: { color }"/>
     </div>
+    <!-- {color} : {_color} -->
     <span class="label">
         {label}
     </span>
 </div>
 
-{#if tooltip && hover}
-  <div id="tooltip" in:fade="{{ duration: 100, delay: 400 }}" out:fade="{{duration: 100}}" class:hover use:popperContent={extraOpts}>
-    {tooltip}
-    <div id="arrow" data-popper-arrow />
-  </div>
-{/if} -->
-
-
 
 <style>
-.container {
-    display: flex;
-    width: 100%;
-    align-items: center;
-    gap: 6px;
-    position: relative;
-}
+
 .swatch {
     width: 22px;
     height: 18px;
@@ -94,77 +144,11 @@
     height: calc(100% - 2px);
     margin: 1px;
 }
-#tooltip {
-    padding: 2px 4px;
-    background-color: var(--tooltip-bg);
-    color: var(--tooltip-color);
-    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.2);
-    z-index: 1;
-    position: absolute;
-    margin-top: 24px;
-}
-.scrim {
-    position: absolute;
-    top: 0;
-  width: 100%;
-  height: 100%;
-  background: rgba(0,0,0,0.5);
-}
-:global(.color-picker) {
+.picker-wrapper {
+    display: flex;
+    align-items: center;
+    gap: 8px;
     margin-right: 8px;
-}
-:global(.color-picker>label) {
-    margin: auto 0;
-    border-radius: 0;
-    background: none;
-}
-:global(.color-picker>label>div) {
-    margin: auto 0;
-    border-radius: 0;
-    background: none;
-    border: 1px solid purple;
-    /* border: 1px solid var(--color-btn-border); */
-
-    width: 22px;
-    height: 18px;
-    border: 1px solid var(--color-btn-border);
-}
-:global(.color-picker>label>div>input[type="color"]) {
-    outline: none;
-    appearance: none;
-    -moz-appearance: none;
-    -webkit-appearance: none;
-    background: none;
-    border: none;
-    cursor: pointer;
-    padding: 0;
-
-    width: calc(100% - 2px);
-    height: calc(100% - 2px);
-    margin: 1px;
-}
-
-:global(::-webkit-color-swatch-wrapper) {
-  padding: 0;
-}
-
-:global(::-webkit-color-swatch) {
-  border: 0;
-  border-radius: 0;
-}
-
-:global(::-moz-color-swatch,
-::-moz-focus-inner) {
-  border: 0;
-}
-
-:global(::-moz-focus-inner) {
-  padding: 0;
-}
-
-
-:global(.color-picker>.wrapper) {
-    background-color: var(--color-btn-icon-hover-border);
 }
 
 </style>
