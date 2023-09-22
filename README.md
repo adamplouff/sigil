320e782b2104b50159d8b3a8708f81b175dd5bb9
Adam Plouff <adamplouff@gmail.com>
2023-09-22 16:36:35 -0400

    update readme

--- README.md
+++ README.md
@@ -3,3 +3,53 @@
 An ongoing Svelte UI component library for [Bolt](https://github.com/hyperbrew/bolt-cep). This is an internal [Battle Axe](https://www.battleaxe.co/) project and will be changing.
 
 There will be no external support. Use at your own risk. 
+
+
+## Basic usage
+
+```svelte
+// main.svelte
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { version } from "../../../package.json";
+  import {
+    csi,
+    evalES,
+    evalFile,
+    subscribeBackgroundColor,
+  } from "../lib/utils";
+
+  import { Panel, Fold, Button, Input, Row, Divider, PanelInfo, Footer, Update, TreeView, Menus, Dropzone, ColorPicker, Toggle, Auth, Modal, Select, Icon } from '@battleaxe/sigil'
+  let appLocale = csi.getHostEnvironment().appLocale?.split("_")[0]
+  let backgroundColor: string = "#202020";
+
+  onMount(() => {
+    if (window.cep) {
+      subscribeBackgroundColor((c: string) => (backgroundColor = c));
+      }
+  })
+</script>
+
+
+<div class="app">
+  <Panel backgroundColor={ backgroundColor }>
+
+  <Menus refresh locale={ appLocale } menuItems={[
+    {
+      label: lang.openStylesFolder,
+      callback: () => evalES(`openStyleFolder()`),
+    },
+  ]}/>
+  <Fold label={lang.fold_system} open={false} prefsId={'foldSystem'}>
+    <Row fill column={ !prefs.mysticMode && windowWidth < minWindowWidth}>
+      <Button label={ lang.updateHoses } tooltip={ lang.updateHoses_tooltip }
+        on:click={() => evalEsWithLoader('upgradeExpressions', prefs) } />
+      />
+      <Button label={ lang.newsletter } tooltip={ lang.newsletter_tooltip }
+        on:click={event => learnStuff(event, 'https://link.battleaxe.co/subscribe') } 
+      />
+    </Row>
+    <PanelInfo name="Rubberhose" version={ version } releaseYear="2015" />
+  </Fold>
+</div>
+```
---
+++ components/System.svelte
@@ -0,0 +1,65 @@
+<script>
+  export let name
+  export let version
+  export let releaseYear
+  import { PanelInfo } from './PanelInfo.svelte'
+  import { Button } from './Button.svelte'
+  import { Toggle } from './Toggle.svelte'
+  import { Row } from './Row.svelte'
+  import { Modal } from './Modal.svelte'
+
+  $: showFooterMessage = false
+
+  let footerText    // local var to store 
+
+  let footerTimeout
+  $: if (footerMessage) {
+    footerText = footerMessage
+    clearTimeout(footerTimeout)
+
+    showFooterMessage = false
+    setTimeout(() => {
+      showFooterMessage = true
+    }, 200);
+
+
+    footerTimeout = setTimeout(() => {
+      showFooterMessage = false
+    }, timeout);
+  }
+</script>
+
+<!-- <div style="margin-bottom: 8px">
+  <Toggle label={ lang.checkUpdates } bind:state={ prefs.checkUpdates } on:change={ evt => updatePref(evt, 'checkUpdates') } />
+  <Toggle label={ lang.mysticMode} bind:state={ prefs.mysticMode } on:change={ evt => updatePref(evt, 'mysticMode') } />
+</div>
+
+<Modal 
+  label={ lang.deauthMessage }
+  confirm={{ 
+    cancel: lang.cancel, 
+    confirm: lang.deauthorize }} 
+  show={ deauthModal } 
+  on:close={() => deauthModal = false } 
+  on:confirm={ deauthorizeKey } 
+  >
+</Modal>
+<Row fill column={ !prefs.mysticMode && windowWidth < minWindowWidth}>
+  <Button label={ lang.updateHoses } tooltip={ lang.updateHoses_tooltip }
+    on:click={() => evalEsWithLoader('upgradeExpressions', prefs) } />
+  
+  <Button label={ lang.deauth } tooltip={ lang.deauth_tooltip }
+    on:click={() => deauthModal = true}></Button>
+
+  <Button label={ lang.learnStuff } tooltip={ lang.learnStuff_tooltip }
+    on:click={event => learnStuff(event, 'https://link.battleaxe.co/rubberhose-manual') } 
+  />
+  <Button label={ lang.newsletter } tooltip={ lang.newsletter_tooltip }
+    on:click={event => learnStuff(event, 'https://link.battleaxe.co/subscribe') } 
+  />
+</Row> -->
+<PanelInfo name={ name } version={ version } releaseYear="2015" />
+
+<style>
+
+</style>
