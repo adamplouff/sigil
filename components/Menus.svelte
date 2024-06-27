ea7009738ddb0d6aa3903d930b424cf6d5b71c39
Adam Plouff <adamplouff@gmail.com>
2024-06-27 10:36:41 -0400

    Line disabling

--- components/Menus.svelte
+++ components/Menus.svelte
@@ -15,8 +15,15 @@
 
   import { csi } from '../lib/utils'
   export let refresh = false
-  export let menuItems = []
+  export let menuItems: {
+    label: string,
+    enabled?: boolean,
+    checkable?: boolean,
+    checked?: boolean,
+    callback: () => void
+  }[] = []
   export let locale = 'en'
+  
 
   const lang = {
     'en': { },
@@ -51,91 +58,92 @@
   lang.fr.reload = 'Panneau de rechargement'
   //#endregion
 
-  if (window.__adobe_cep__) {
 
-    const refreshPage = () => {
-        location.reload()
-    }
+  const refreshPage = () => {
+      location.reload()
+  }
+
+  let contextMenu = {
+    menu: [ ]
+  }
   
-    let contextMenu = {
-      menu: [ ]
-    }
+
+  const callback = (event) => {
+    let clickedItem
+    contextMenu.menu.forEach(menuItem => {
+      if (menuItem.id == event) {
+        clickedItem = menuItem
+      }
+    })      
+    clickedItem.callback()
+  }
+  const flyoutCallback = (event) => {
+    callback(event.data.menuId)
+  }
+
+  const buildContextMenu = (menuItems) => {
+    contextMenu.menu = []
     
-  
-    const callback = (event) => {
-      let clickedItem
-      contextMenu.menu.forEach(menuItem => {
-        if (menuItem.id == event) {
-          clickedItem = menuItem
-        }
+    if (refresh) { contextMenu.menu.push({
+        id: 'refresh',
+        label: _lang.reload,
+        enabled: true,
+        checkable: false,
+        checked: false,
+        callback: refreshPage,
       })
-      clickedItem.callback()
-    }
-    const flyoutCallback = (event) => {
-      callback(event.data.menuId)
     }
 
-    const buildContextMenu = () => {
-      if (refresh) { contextMenu.menu.push({
-          id: 'refresh',
-          label: _lang.reload,
-          enabled: true,
-          checkable: false,
-          checked: false,
-          callback: refreshPage,
+    contextMenu.menu.push({ label: '---' })
+
+    if (menuItems.length > 0) {
+      menuItems.forEach(item => { 
+        contextMenu.menu.push({
+          id: item.label.replace(' ', '_'),
+          label: item.label,
+          enabled: (item.enabled !== undefined) ? item.enabled : true,
+          checkable: item.checkable || false,
+          checked: item.checked || false,
+          callback: item.callback,
         })
-      }
+      })
+    }    
+  }
+  const buildFlyoutMenuItemInXML = (item) => {      
+    let str = ``;
+    if (item.id) {
+      str += `<MenuItem Id="${item.id}" Label="${item.label}" Enabled="${
+        item.enabled || "true"
+      }" Checkable="${item.checkable || "false"}" Checked="${
+        item.checked || "false"
+      }"`;
+      if (item.menu && item.menu.length) {
+        str += ">";
+        item.menu.forEach((child) => {
+          str += buildFlyoutMenuItemInXML(child);
+        });
+        str += "</MenuItem>";
+      } else str += "/>";
+    } else str += `<MenuItem Label="---" />`;
+    return str;
+  }
+  const buildFlyoutMenuInXML = () =>  {
+    let str = `<Menu>`;
+    contextMenu.menu.forEach((item, i) => {
+      str += buildFlyoutMenuItemInXML(item, i);
+    });      
+    return (str += `</Menu>`);
+  }
 
-      contextMenu.menu.push({ label: '---' })
-
-      if (menuItems.length > 0) {
-        menuItems.forEach(item => {
-            contextMenu.menu.push({
-            id: item.label.replace(' ', '_'),
-            label: item.label,
-            enabled: item.enabled || true,
-            checkable: item.checkable || false,
-            checked: item.checked || false,
-            callback: item.callback,
-          })
-        })
-      }
-    }
-    const buildFlyoutMenuItemInXML = (item) => {
-      let str = ``;
-      if (item.id) {
-        str += `<MenuItem Id="${item.id}" Label="${item.label}" Enabled="${
-          item.enabled || "true"
-        }" Checkable="${item.checkable || "false"}" Checked="${
-          item.checked || "false"
-        }"`;
-        if (item.menu && item.menu.length) {
-          str += ">";
-          item.menu.forEach((child) => {
-            str += buildFlyoutMenuItemInXML(child);
-          });
-          str += "</MenuItem>";
-        } else str += "/>";
-      } else str += `<MenuItem Label="---" />`;
-      return str;
-    }
-    const buildFlyoutMenuInXML = () =>  {
-      let str = `<Menu>`;
-      contextMenu.menu.forEach((item, i) => {
-        str += buildFlyoutMenuItemInXML(item, i);
-      });
-      return (str += `</Menu>`);
-    }
-  
-    onMount(() => {
-      buildContextMenu()
-
-      csi.setContextMenuByJSON(JSON.stringify(contextMenu), callback)
-      csi.setPanelFlyoutMenu(buildFlyoutMenuInXML(contextMenu))
-      csi.addEventListener("com.adobe.csxs.events.flyoutMenuClicked", flyoutCallback)
-    })
+  $: {
+    buildContextMenu(menuItems)
+    
+    
+    csi.setContextMenuByJSON(JSON.stringify(contextMenu), callback)
+    csi.setPanelFlyoutMenu(buildFlyoutMenuInXML(contextMenu))
+    csi.addEventListener("com.adobe.csxs.events.flyoutMenuClicked", flyoutCallback)
   }
 
 </script>
 
-<div v-if="isAdobe" class="menu"></div>
+<div class="menu"></div>
